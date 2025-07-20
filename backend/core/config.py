46856a83dcf83ec9ad913c93c83e0e0504d8680e
Anirudh <anirudh.singh@thoughtworks.com>
2025-07-20 13:57:08 +0530

    Move text_gen system prompt to config

--- backend/core/config.py
+++ backend/core/config.py
@@ -5,12 +5,30 @@ class Config:
     deepinfra_base_url: str
     deepinfra_api_key: str
     model: str
+    text_gen_system_prompt: str = '''
+        You are a creative story writer and an image prompt writer, that generates fictional stories alongwith intricate level details of each paragraph in structured JSON format.
+
+        Always follow this output format strictly:
+        {
+          "summary": "A short 100-word summary of the story",
+          "contents": [
+            { "text": "Paragraph 1", "image_prompt": "A very detailed decriptions of what this scene would look like if represented in form of an image prompt" },
+            { "text": "Paragraph 2", "image_prompt": "A very detailed decriptions of what this scene would look like if represented in form of an image prompt" },
+            ...depends upon the number of paragraph user wants
+          ]
+        }
+
+        Write stories based on the given input: genre, character names/number of characters (either may be present), and number of paragraphs.
+
+        Make the story vivid, imaginative, emotionally engaging, and coherent. The story must reflect the genre clearly and develop the characters throughout the paragraphs. If character names are not provided, make appropriate ones.
+
+        Do not explain anything, just return the final JSON object.
+    '''
 
     def __init__(self):
         self.deepinfra_base_url = os.getenv("DEEPINFRA_BASE_URL")
         self.deepinfra_api_key = os.getenv("DEEPINFRA_API_KEY")
         self.model = os.getenv("MODEL")
-        print("Config updated successfully!")
 
 @lru_cache
 def get_config():
--- backend/services/content_generation_service.py
+++ backend/services/content_generation_service.py
@@ -1,4 +1,5 @@
 from ..core.deep_infra_llm_client import DeepInfraLLMClient
+from ..core.config import get_config
 from ..schemas.story_request import StoryRequest
 from ..schemas.story_response import StoryResponse
 from ..utils.get_story_prompt import get_story_prompt
@@ -8,35 +9,16 @@ class ContentGenerationService():
 
     def __init__(self):
         self.text_generation_client = DeepInfraLLMClient()
-    
-    text_gen_system_prompt: str = '''
-        You are a creative story writer and an image prompt writer, that generates fictional stories alongwith intricate level details of each paragraph in structured JSON format.
-
-        Always follow this output format strictly:
-        {
-          "summary": "A short 100-word summary of the story",
-          "contents": [
-            { "text": "Paragraph 1", "image_prompt": "A very detailed decriptions of what this scene would look like if represented in form of an image prompt" },
-            { "text": "Paragraph 2", "image_prompt": "A very detailed decriptions of what this scene would look like if represented in form of an image prompt" },
-            ...depends upon the number of paragraph user wants
-          ]
-        }
-
-        Write stories based on the given input: genre, character names/number of characters (either may be present), and number of paragraphs.
-
-        Make the story vivid, imaginative, emotionally engaging, and coherent. The story must reflect the genre clearly and develop the characters throughout the paragraphs. If character names are not provided, make appropriate ones.
-
-        Do not explain anything, just return the final JSON object.
-    '''
 
 
     def generate_story(self, req: StoryRequest) -> StoryResponse | None:
+        config = get_config()
         prompts = []
 
         # Add system prompt
         prompts.append({
             "role": "system",
-            "content": self.text_gen_system_prompt,
+            "content": config.text_gen_system_prompt,
         })
 
         # Add user prompt
